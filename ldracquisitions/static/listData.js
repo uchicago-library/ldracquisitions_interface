c98f1fa6e3158d22c92dc50bc83be4ed94951ebe
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-10-21 09:28:38 -0500

    rewrote whole application; encaspulated all the different form pages into one apge and the 2 list pages into a single page. each type of page (form and list) uses a distinct code base

---
+++ ldracquisitions/static/listData.js
@@ -0,0 +1,193 @@
+/*global window*/
+/*global alert*/
+/*global $*/
+/*global confirm*/
+
+var ajaxURL = "https://y2.lib.uchicago.edu/hrapi/";
+
+$(document).ready(function () {
+    "use strict";
+
+    function displayAWord(aString) {
+        var firstLetter = aString[0].toUpperCase();
+        var restOfString = aString.slice(1, aString.length);
+        return firstLetter + restOfString;
+    }
+
+    function getAValueInARecord(recordId, key) {
+        var urlString = ajaxURL + "record/" + recordId + "/" + encodeURIComponent(key);
+        var newObj = Object.create(null);
+        return $.ajax({
+            type: "GET",
+            url: urlString,
+            data: JSON.stringify(newObj),
+            async: false,
+            success: function(data) {return data;},
+            contenType: "application/json",
+            dataType: "json"
+        });
+    }
+
+    function getRecord(recordID) {
+        var urlString = ajaxURL + "record/" + recordID;
+        var newObj = Object.create(null);
+        return $.ajax({
+            type: "GET",
+            url: urlString,
+            data: JSON.stringify(newObj),
+            async: false,
+            success: function(data) {
+                return data;
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        });
+    }
+
+
+    function getRecordsByCategory(categoryName) {
+        var urlString = ajaxURL + "category/" + categoryName;
+        var newObj = Object.create(null);
+        return $.ajax({
+            type: "GET",
+            url: urlString,
+            data: JSON.stringify(newObj),
+            async: false,
+            success: function(data) {return data;},
+            contentType: "application/json",
+            dataType: "json"
+        });
+    }
+
+    function getURLQueryParams() {
+        var url = window.location;
+        var searchParams = url.search;
+        var out = null;
+        if (searchParams.indexOf("&") > -1) {
+            out = searchParams.split("?")[1].split("&");
+        } else {
+            out = searchParams.split("?"); //.split("&");
+        }
+        return out;
+    }
+
+    function findStringInArray(aList, stringValue) {
+        var cur = null;
+        var out = null;
+        var i = 0;
+        for (i = 0; i < aList.length; i += 1) {
+            cur = aList[i];
+            if (cur.indexOf(stringValue) > -1) {
+                out = cur;
+            }
+        }
+        return out;
+    }
+
+    $(function() {
+        $("button[id^=transform-").click(function () {
+            var yourChoice = $(this).attr("id").split("transform-")[1];
+            window.location.replace("form.html?action=accession&item=" + yourChoice);
+        });
+    });
+
+    $(function() {
+        $("button[id^='export-']").click(function() {
+            var id = this.getAttribute("id").split("export-")[1];
+	    var n = "div[class='export-" + id + "']";
+	    var d = getRecord(id);
+	    var p = $(n).html(JSON.stringify(d.responseJSON.data.record));
+        });
+    });
+
+
+    localStorage.clear()
+    var params = getURLQueryParams();
+    var decision = findStringInArray(params, "action=");
+    if (decision === "") {
+        decision = null;
+    } else {
+        decision = decision.split("action=")[1];
+    }
+    var data = getRecordsByCategory(displayAWord(decision) + "Record");
+    var ids = data.responseJSON.data.record_identifiers;
+    var htmlToFill = $("#listing-div");
+    var header = $("#listing-header");
+    header.html(document.createTextNode("List of " + displayAWord(decision)+"s"));
+    var dl = document.createElement("dl");
+    dl.setAttribute("class", "list-group"); 
+    var count = 0;
+    if (decision === 'accession') {
+        for (var n in ids) {
+            var value = ids[n];
+            var exportButton = document.createElement("button");
+            exportButton.setAttribute("class", "btn btn-primary btn-sm");
+            exportButton.setAttribute("role", "button")
+            exportButton.setAttribute("id", "export-" + value);
+            exportButton.appendChild(document.createTextNode("Export"));
+	    var dd = document.createElement("dd");
+	    dd.appendChild(exportButton);
+	    dd.appendChild(document.createTextNode(" "));
+            var dt = document.createElement("dt");
+            var title = getAValueInARecord(value, "Collection Title").responseJSON.data.value[0];
+            var accessionid = getAValueInARecord(value, "Accession Identifier").responseJSON.data.value[0];
+            var val = accessionid + " from " + title;
+	    var listItemDiv = document.createElement("div");
+	    var exportDiv = document.createElement("div");
+	    exportDiv.setAttribute("class", "export-" + value);
+
+	    listItemDiv.setAttribute("class", "list-group-item");
+	    listItemDiv.setAttribute("id", value);
+            dt.appendChild(document.createTextNode(val));
+            listItemDiv.appendChild(dt);
+	    listItemDiv.appendChild(dd);
+	    listItemDiv.appendChild(exportDiv);
+            dl.appendChild(listItemDiv);
+            count += 1;
+        }
+    } else if (decision == 'acquisition') {
+        for (var n in ids) {
+            var value = ids[n];
+            var exportButton = document.createElement("button");
+            exportButton.setAttribute("class", "btn btn-primary btn-sm");
+            exportButton.setAttribute("role", "button")
+            exportButton.setAttribute("id", "export-" + value);
+            exportButton.appendChild(document.createTextNode("Export"));
+	
+	    var transformButton = document.createElement("button");
+            transformButton.setAttribute("class", "btn btn-primary btn-sm");
+            transformButton.setAttribute("role", "button")
+            transformButton.setAttribute("id", "transform-" + value);
+            transformButton.appendChild(document.createTextNode("Transform to Accession"));
+	
+	    var dd = document.createElement("dd");
+	    dd.appendChild(exportButton);
+	    dd.appendChild(document.createTextNode(" "));
+	    dd.appendChild(transformButton);
+            var dt = document.createElement("dt");
+            var title = getAValueInARecord(value, "Collection Title").responseJSON.data.value[0];
+            var accessionid = getAValueInARecord(value, "Accession Identifier").responseJSON.data.value[0];
+            var val = accessionid + " from " + title;
+	    var listItemDiv = document.createElement("div");
+	    var exportDiv = document.createElement("div");
+	    exportDiv.setAttribute("class", "export-" + value);
+
+
+	    listItemDiv.setAttribute("class", "list-group-item");
+	    listItemDiv.setAttribute("id", value);
+            dt.appendChild(document.createTextNode(val));
+            listItemDiv.appendChild(dt);
+	    listItemDiv.appendChild(dd);
+	    listItemDiv.appendChild(exportDiv);
+            dl.appendChild(listItemDiv);
+            count += 1;
+       }
+    }
+    if (count == 0) {
+       var p = document.createElement("p");
+       p.appendChild(document.createTextNode("There are no records at this time."));
+       htmlToFill.html(p)
+    } else {
+       htmlToFill.html(dl);
+    }
+});
